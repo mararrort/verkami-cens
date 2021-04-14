179397ca6f3dbfe1bbb64b7c31ed1268e83bf5f8
Mar <mararrort@users.noreply.github.com>
2021-04-14 18:44:58 +0000

    Apply fixes from StyleCI

--- app/Notifications/PetitionAccepted.php
+++ app/Notifications/PetitionAccepted.php
@@ -9,11 +9,11 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Log;
 use NotificationChannels\Telegram\TelegramChannel;
 use NotificationChannels\Telegram\TelegramMessage;
 use NotificationChannels\Twitter\TwitterChannel;
 use NotificationChannels\Twitter\TwitterStatusUpdate;
-use Illuminate\Support\Facades\Log;
 
 class PetitionAccepted extends Notification
 {
@@ -66,23 +66,23 @@ class PetitionAccepted extends Notification
      **/
     public function toTwitter($notifiable)
     {
-        $tweet = "";
+        $tweet = '';
         if ($this->petition->isUpdate()) {
-            $tweet = "Se ha actualizado la información sobre el mecenazgo "
-                . $this->presale->name . " de la editorial " . $this->editorial->name
-                . ".";
+            $tweet = 'Se ha actualizado la información sobre el mecenazgo '
+                .$this->presale->name.' de la editorial '.$this->editorial->name
+                .'.';
         } else {
-            $tweet = "Se ha añadido la información sobre el mecenazgo "
-            . $this->petition->presale_name . " de la editorial " . $this->petition->editorial_name
-            . ".";
+            $tweet = 'Se ha añadido la información sobre el mecenazgo '
+            .$this->petition->presale_name.' de la editorial '.$this->petition->editorial_name
+            .'.';
         }
 
-        $tweet = $tweet . " Tienen " . (string)($this->petition->isNewNotFinished() ? count($editorial->getNotFinishedPresales()) + 1 : count($this->editorial->getNotFinishedPresales()))
-            . " juegos pendientes de entregar, y " . (string)(($this->petition->isNewNotFinished() && $this->petition->isNewLate()) ? count($this->editorial->getNotFinishedLatePresales()) + 1 : count($this->editorial->getNotFinishedLatePresales()))
-            . " pendientes y con retraso.";
+        $tweet = $tweet.' Tienen '.(string) ($this->petition->isNewNotFinished() ? count($editorial->getNotFinishedPresales()) + 1 : count($this->editorial->getNotFinishedPresales()))
+            .' juegos pendientes de entregar, y '.(string) (($this->petition->isNewNotFinished() && $this->petition->isNewLate()) ? count($this->editorial->getNotFinishedLatePresales()) + 1 : count($this->editorial->getNotFinishedLatePresales()))
+            .' pendientes y con retraso.';
 
-        Log::info("This is the tweet that will be send", ['tweet', $tweet]);
-        
+        Log::info('This is the tweet that will be send', ['tweet', $tweet]);
+
         return new TwitterStatusUpdate($tweet);
     }
 }
