c9e84ed73023816416f128190c51dbb58ad8e4b6
Mar Arribas <10339571+mararrort@users.noreply.github.com>
2021-04-15 10:58:12 +0200

    Added cascade to models

---
+++ database/migrations/2021_04_15_083825_cascade_remove_the_models.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CascadeRemoveTheModels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('presales', function (Blueprint $table) {
+            $table->foreign('editorial_id')
+                ->references('id')
+                ->on('editorials')
+                ->onDelete('cascade')
+                ->change();
+        });
+        Schema::table('petitions', function (Blueprint $table) {
+            $table->foreign('editorial_id')
+                ->references('id')
+                ->on('editorials')
+                ->onDelete('cascade')
+                ->change();
+            
+            $table->foreign('presale_id')
+                ->references('id')
+                ->on('presales')
+                ->onDelete('cascade')
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('presales', function (Blueprint $table) {
+            $table->foreign('editorial_id')
+                ->references('id')
+                ->on('editorials')
+                ->change();
+        });
+        Schema::table('petitions', function (Blueprint $table) {
+            $table->foreign('editorial_id')
+                ->references('id')
+                ->on('editorials')
+                ->change();
+            
+            $table->foreign('presale_id')
+                ->references('id')
+                ->on('presales')
+                ->change();
+        });
+    }
+}
---
+++ tests/Feature/CascadeRemoveTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\Editorial;
+use App\Models\Presale;
+use App\Models\Petition;
+
+class CascadeRemoveTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_can_remove_editorial()
+    {
+        // Creates the material
+        $editorial = Editorial::factory()->create();
+        $presales = Presale::factory(8)->for($editorial)->create();
+        foreach ($presales as $presale) {
+            Petition::factory()->presale($presale)->create();
+        }
+        Petition::factory(8)->editorial($editorial)->create();
+
+        // Deletes the editorial
+        $editorial->delete();
+
+        // Check there is nothing in the DDBB
+        $this->assertEmpty(Editorial::all());
+        $this->assertEmpty(Presale::all());
+        $this->assertEmpty(Petition::all());
+    }
+}
