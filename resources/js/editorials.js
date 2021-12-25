d242c4012f7a37edbbc4a929e996dcf073aac750
Mar Arribas <mar@example.com>
2021-12-25 19:59:36 +0100

    Datatables in spanish

--- resources/js/editorials.js
+++ resources/js/editorials.js
@@ -1,6 +1,9 @@
 import 'datatables.net-bs4'
 $( function () {
     $("#editorialsTableId").DataTable({
+        "language": {
+            "url": 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json'
+        },
         "order": [ 1, 'desc' ]
     });
 } );
--- resources/js/presales.js
+++ resources/js/presales.js
@@ -2,6 +2,9 @@ import 'datatables.net-bs4'
 
 $( function () {
     $("#presalesTableId").DataTable({
+        "language": {
+            "url": 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json'
+        },
         "order": [ 4, 'desc' ]
     });
 } );
